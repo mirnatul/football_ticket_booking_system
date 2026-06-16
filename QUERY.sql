b3282823819b59eacc2acdda015a27cde558c551
mirnatul <mdmirnatul@gmail.com>
2026-06-16 16:43:31 +0600

    query 2 completed

--- QUERY.sql
+++ QUERY.sql
@@ -101,7 +101,9 @@ select match_id, fixture, base_ticket_price::int from matches
   where tournament_category = 'Champions League' and match_status = 'Available';
 
 
-
+-- ================Query 2
+select user_id, full_name, email from users 
+  where full_name ilike 'Tanvir%' or full_name ilike '%Haque';
 
 
 
